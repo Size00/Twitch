40c0342e2237f56711a0505b16eb37dab5611921
Size <55809336+Size00@users.noreply.github.com>
2020-04-04 23:00:19 -0500

    Create twitchAPI.php
    /////
    ////// This script gets twitch users ID which is needed for other api request. 
    /////

---
+++ twitchAPI.php
@@ -0,0 +1,13 @@
+function get_TwitchUser($username){
+$curl_h = curl_init('https://api.twitch.tv/helix/users?login=garytheproducer');
+curl_setopt($curl_h, CURLOPT_HTTPHEADER,
+ array('method' => "GET",
+       'Accept: application/vnd.twitchtv.v5+json',
+       'Client-id: *tqwon3********z2ux39a******p',
+       'Authorization: OAuth 4*******a*v******8exq**a'	)); // create an accoount on twitch to get their clientID & OAuth key...
+curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
+$response = json_decode(curl_exec($curl_h), true);
+$tID = $response['data']['0']['id'];
+echo 'Twitch ID - '. $tID .'</br> Username - '. $tlogin;
+}
+get_TwitchUser('garytheproducer'); // change username to whatever the hell you want...

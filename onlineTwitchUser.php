34981848f630e34a97d9dab8683b3b0ee1e3d3ab
Size <55809336+Size00@users.noreply.github.com>
2020-04-05 16:18:02 -0500

    Create onlineTwitchUser.php

---
+++ onlineTwitchUser.php
@@ -0,0 +1,17 @@
+function get_online_TwitchUser($username){
+$curl_h = curl_init('https://api.twitch.tv/helix/streams?user_id='.$username);
+curl_setopt($curl_h, CURLOPT_HTTPHEADER,
+array(
+	   	'Accept: application/vnd.twitchtv.v5+json',
+      'Client-id: ******************************'       	
+      )
+);
+curl_setopt($curl_h, CURLOPT_RETURNTRANSFER, true);
+$jd_curl = json_decode(curl_exec($curl_h), true);
+$user_name = $jd_curl['data']['0']['user_name'];
+if($jd_curl['data'] != $jd_curl['data']['0']){
+	echo $user_name.' is online';
+}
+}
+
+get_online_TwitchUser('162509222'); // change to the twitch user id that you're looking for...
